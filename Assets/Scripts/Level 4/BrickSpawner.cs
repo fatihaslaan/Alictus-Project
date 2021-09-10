72d6de645881323eb872dc750b94a13b7ca6dec1
Fatih Aslan <fatihaslan9948@gmail.com>
2021-09-10 03:01:00 +0300

    Level 4 Completed

---
+++ Assets/Scripts/Level 4/BrickSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickSpawner : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> brickTypes; //Brick types that we want to spawn
+
+    [SerializeField]
+    int totalRow, totalColumn; //Brick size
+
+    [SerializeField]
+    float brickAreaLeftLimit, brickAreaRightLimit, brickAreaBottomLimit, brickAreaTopLimit; //To create nodes in birck area
+
+    float columnGap, rowGap; //Gap between columns and rows
+
+    void Start()
+    {
+        for (int i = 0; i < totalRow; i++)
+        {
+            for (int j = 0; j < totalColumn; j++)
+            {
+                columnGap = (brickAreaRightLimit - brickAreaLeftLimit) / (totalColumn - 1); //Calculate gap
+                rowGap = (brickAreaTopLimit - brickAreaBottomLimit) / (totalRow - 1);
+
+                Instantiate(brickTypes[Random.Range(0, brickTypes.Count)], new Vector3(brickAreaLeftLimit + (columnGap * j), 0, brickAreaTopLimit - (rowGap * i)), Quaternion.identity).GetComponent<Rigidbody>().isKinematic=true;
+                Ball.totalBricks++;
+            }
+        }
+    }
+}
